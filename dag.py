8ab7248acbfb79fb4954945a5c51d8ca29c2d718
Bavanan <151564927+bobbramillan@users.noreply.github.com>
2025-11-10 15:27:57 -0500

    Create dag.py

---
+++ dag.py
@@ -0,0 +1,12 @@
+courses = {
+    "CS 164": [],
+    "CS 171": [],
+    "CS 172": [{"course": "CS 171", "min_grade": "C"}],
+    "CS 265": [{"course": "CS 172", "min_grade": "C"}],
+    "CS 260": [{"course": "CS 265", "min_grade": "B"}],
+    "CS 270": [{"course": "CS 172", "min_grade": "B"}],
+    "CS 300": [
+        {"course": "CS 265", "min_grade": "B"},
+        {"course": "CS 270", "min_grade": "C"}
+    ]
+}
